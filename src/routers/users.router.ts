3e75c2d489345fb2a701270ab91207ac76238754
GiovanneAndrade <vanne.as@hotmail.com>
2022-11-07 18:43:09 -0300

    :sparkles: feat: add routers users

---
+++ src/routers/users.router.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { postCategoryController } from "../controllers/users.controller";
+
+const router = Router();
+
+router.post("/user", postCategoryController);
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import categoryRouter from "./routers/category.router";
+import categoryRouter from "./routers/users.router";
 
 const app = express();
 app.use(express.json());
