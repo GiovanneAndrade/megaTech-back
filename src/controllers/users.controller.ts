cf2b19beb566852005e190fbee9feccf4464931d
GiovanneAndrade <vanne.as@hotmail.com>
2022-11-07 19:12:40 -0300

    :sparkles: feat: add user controllers

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -2,9 +2,23 @@ import * as allCategory from "../repositories/users.repository";
 import { Request, Response } from "express";
 
 async function postCategoryController(req: Request, res: Response) {
-  const result = await allCategory.postCategoryRepository();
+  const {
+    name,
+    email,
+    password,
+    cpf,
+    phone,
+  }: { name: string; email: string; password: string; cpf: string , phone: number } =
+    req.body;
+  const result = await allCategory.postCategoryRepository({
+    name,
+    email,
+    password,
+    cpf,
+    phone,
+  });
 
   return res.send(result);
 }
 
-export {  postCategoryController };
+export { postCategoryController };
