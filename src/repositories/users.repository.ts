2032aa63d6ab6d91a6ece55b9c067070a2f2cf84
GiovanneAndrade <vanne.as@hotmail.com>
2022-11-07 19:13:03 -0300

    :sparkles: feat: add user repositories

--- src/repositories/users.repository.ts
+++ src/repositories/users.repository.ts
@@ -2,11 +2,26 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function postCategoryRepository() {
+async function postCategoryRepository({
+  name,
+  email,
+  password,
+  cpf,
+  phone,
+}: {
+  name: string;
+  email: string;
+  password: string;
+  cpf: string;
+  phone: number;
+}) {
   const result = await prisma.user.create({
     data: {
-      category: "teste1",
-      image: "testeImage1",
+      name: name,
+      email: email,
+      cpf: cpf,
+      password: password,
+      phone: phone,
     },
   });
   return result;
