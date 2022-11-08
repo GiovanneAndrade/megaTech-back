f462caca659ecdec1a2bac58429630ef4af9b019
GiovanneAndrade <vanne.as@hotmail.com>
2022-11-08 09:19:54 -0300

    :sparkles: feat: add categories repositories

--- src/repositories/categories.repositories.ts
+++ src/repositories/categories.repositories.ts
@@ -2,9 +2,16 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function getCategoryRepository(){
-  const result = await prisma.category.findMany()
-  
+async function getCategoryRepository() {
+  const result = await prisma.category.findMany({
+    select: {
+      id: true,
+      name: true,
+      image: true,
+      products: true,
+    },
+  });
+
   return result;
 }
 
