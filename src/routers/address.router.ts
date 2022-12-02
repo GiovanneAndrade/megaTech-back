6904ada55cd7045311a551222d3a1c5cbfc6897a
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-01 21:24:05 -0300

    :sparkles: feat: add delete  & put address router

--- src/routers/address.router.ts
+++ src/routers/address.router.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
-import { getAddressController, postAddressController } from "../controllers/address.controllers";
-
- 
- 
+import {
+  deleteAddressController,
+  getAddressController,
+  postAddressController,
+  updateAddressController,
+} from "../controllers/address.controllers";
 
 const router = Router();
 
 router.post("/address", postAddressController);
 router.get("/address", getAddressController);
+router.delete("/address/:id", deleteAddressController);
+router.put("/address/:currentAddress/:previousAddress", updateAddressController);
 
 export default router;
