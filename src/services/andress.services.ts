fc5e5fe2ccfe7aef1127f737ead1ce78c0877457
GiovanneAndrade <vanne.as@hotmail.com>
2023-02-22 20:49:17 -0300

    :sparkles:	feat: services address

---
+++ src/services/andress.services.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from "express";
+import * as allAddress from "../repositories/index";
+import { Andress } from "../protocols/index";
+import { consultUser } from "../repositories/index";
+import { conflictError, NotFoundError, UnauthoredError } from "../erros/erros";
+
+async function postAddressServices(andress: Andress): Promise<Andress> {
+  const userId = await consultUser(Number(andress.userId));
+  if (!userId) throw new NotFoundError("usuario não existe");
+
+  const result = await allAddress.postAddressRepository(andress);
+  return result;
+} 
+
+async function getAddressServices() {
+  const result = await allAddress.getAddressRepository();
+  return result;
+}
+
+async function deleteAddressServices(id : any) {
+  const consultAddressById = await allAddress.consultAddressRepository( id );
+  if (!consultAddressById) throw new UnauthoredError("Address não existe");
+
+  const result = await allAddress.deleteAddressRepository( id );
+
+  return result;
+}
+
+async function updateAddressServices( currentAddress:any, previousAddress: any) {
+
+  const consultCurrentAddress = await allAddress.consultAddressRepository( currentAddress );
+  const consultPreviousAddress = await allAddress.consultAddressRepository( previousAddress );
+  if (!consultCurrentAddress || !consultPreviousAddress) throw new NotFoundError("Address não existe");
+
+  const result = await allAddress.updateAddressRepository({currentAddress, previousAddress,});
+  return result;
+}
+
+export {
+  postAddressServices,
+  getAddressServices,
+  deleteAddressServices,
+  updateAddressServices,
+};
