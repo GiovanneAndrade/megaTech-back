f2069a7229080fa3f3620fca6013dc9bb4cf4ebf
GiovanneAndrade <vanne.as@hotmail.com>
2023-02-23 19:33:31 -0300

    :sparkles:	feat:  products server

--- src/services/products.services.ts
+++ src/services/products.services.ts
@@ -1,11 +1,14 @@
 import * as allProducts from "../repositories/index";
 
 
-
+async function getProductsService() {
+  const result = await allProducts.getProductsRepository();
+  return result;
+}
 
 async function getProductsHotService() {
   const result = await allProducts.getProductsHotRepository();
   return result;
 }
 
-export {  getProductsHotService };
+export { getProductsService, getProductsHotService };
