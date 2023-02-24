ac1099a4463c4a260820a5bcc264c4cf3fb256fa
GiovanneAndrade <vanne.as@hotmail.com>
2023-02-24 20:10:01 -0300

    :sparkles:	feat: consult users service

---
+++ src/services/consultUser.services.ts
@@ -0,0 +1,12 @@
+import { NotFoundError } from "../erros/erros";
+
+import * as allUser from "../repositories";
+
+async function consultUserService(userId: number) {
+  const user = await allUser.consultUser(Number(userId));
+  if (!user) throw new NotFoundError("usuario não existe");
+
+  return user;
+}
+
+export { consultUserService };
