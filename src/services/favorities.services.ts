94ce5dfaa1359e39957b8254d0427e371702c40a
GiovanneAndrade <vanne.as@hotmail.com>
2023-02-27 16:41:11 -0300

    :bug:	fix: corrections favorities

--- src/services/favorities.services.ts
+++ src/services/favorities.services.ts
@@ -1,13 +1,22 @@
+import { NotFoundError } from "../erros/erros";
 import { Favorities } from "../protocols";
 import * as allFavorities from "../repositories/index";
+import { consultUserService } from "./consultUser.services";
 
 async function postFavoritiesService(favorities: Favorities) {
+  await consultUserService(favorities.userId)
+  const { id } = favorities.productId[0];
+  const consultProductId = await allFavorities.consultProductsHotRepository(id);
+  if (consultProductId.length === 0) throw new NotFoundError("produto não existe");
+
   const result = await allFavorities.postFavoritiesRepository(favorities);
   return result;
 }
 
-async function getFavoritiesService() {
-  const result = await allFavorities.getFavoritiesRepository();
+async function getFavoritiesService(userId: number) {
+  await consultUserService(userId)
+
+  const result = await allFavorities.getFavoritiesRepository(userId);
   return result;
 }
 
