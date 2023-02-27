c6b34300d11aeb05133b53361b59fef01d9412f6
GiovanneAndrade <vanne.as@hotmail.com>
2023-02-27 16:49:43 -0300

    :bug:	fix: corrections user repositories

--- src/repositories/users.repository.ts
+++ src/repositories/users.repository.ts
@@ -1,30 +1,20 @@
 import { PrismaClient } from "@prisma/client";
+import { User } from "../protocols";
 
 const prisma = new PrismaClient();
 
-async function postCategoryRepository({
-  name,
-  email,
-  password,
-  cpf,
-  phone,
-}: {
-  name: string;
-  email: string;
-  password: string;
-  cpf: string;
-  phone: number;
-}) {
-  const result = await prisma.user.create({
+async function postUsersRepository(user: User) {
+  return await prisma.user.create({
     data: {
-      name: name,
-      email: email,
-      cpf: cpf,
-      password: password,
-      phone: phone,
+      name: user.name,
+      email: user.email,
+      cpf: user.cpf,
+      password: user.password,
+      phone: user.phone,
     },
   });
- 
+}
+
 async function consultUser(userId: any) {
   return await prisma.user.findFirst({
     where: {
@@ -33,4 +23,4 @@ async function consultUser(userId: any) {
   });
 }
 
-export { postCategoryRepository };
+export { postUsersRepository, consultUser };
