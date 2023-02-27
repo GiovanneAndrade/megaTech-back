3ffda15da4ef9fedf5a41991df2efe728e4b29a8
GiovanneAndrade <vanne.as@hotmail.com>
2023-02-27 16:44:18 -0300

    :bug:	fix: corrections request controllers

--- src/controllers/request.controlers.ts
+++ src/controllers/request.controlers.ts
@@ -1,26 +1,25 @@
 import { Request, Response } from "express";
-import { InternalServerError } from "../erros/erros";
-import * as allRequest from "../services/index";
+import { ifNotFoundError, InternalServerError } from "../erros/erros";
+import { Requests } from "../protocols";
+import * as allRequest from "../services";
 
 async function postRequestController(req: Request, res: Response) {
   const lastRequest = req.body as Requests;
- 
-  const lastRequest = req.body;
+
   try {
     const summary = await allRequest.postRequestService(lastRequest);
     return res.send(summary);
   } catch (error: any) {
     if (error.statusCode === 404) return ifNotFoundError(res, error);
 
-    return res.send( summary );
-  } catch (error) {
     return InternalServerError(res);
   }
 }
 
 async function getRequestController(req: Request, res: Response) {
+  const id = req.params.id;
   try {
-    const result = await allRequest.getRequestService();
+    const result = await allRequest.getRequestService(Number(id));
     return res.send(result);
   } catch (error: any) {
     if (error.statusCode === 404) return ifNotFoundError(res, error);
