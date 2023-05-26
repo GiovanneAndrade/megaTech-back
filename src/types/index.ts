bb2697381a5cb33dad59c72bf5caec58f956fc16
GiovanneAndrade <vanne.as@hotmail.com>
2023-05-26 17:31:43 -0300

    :label: index types

---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export * from "./address.protocols";
+export * from "./users.protocols"
+export * from "./favorities.protocols"
+export * from "./request.protocols"
+export * from "./products.protocols"
