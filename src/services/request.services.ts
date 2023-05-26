2a1173859da2d491c692e05d94889ca9e4a67f5b
GiovanneAndrade <vanne.as@hotmail.com>
2023-05-26 17:34:05 -0300

    :sparkles:	fix: user service tweaks

--- src/services/request.services.ts
+++ src/services/request.services.ts
@@ -1,22 +1,35 @@
 import { NotFoundError } from "../erros/erros";
-import { Requests } from "@/protocols";
+import { Requests } from "../types";
 import * as allRepositories from "../repositories";
 import { consultUserService } from "./consultUser.services";
 
-
-async function postRequestService(lastRequest: Requests) {
+async function postRequestService(lastRequest: Requests, userId: number) {
+  if (lastRequest.userId !== userId) {
+    throw new NotFoundError("usuario não autorizado");
+  }
 
   await consultUserService(lastRequest.userId);
 
-  const consultAddress = await allRepositories.consultAddressRepository(lastRequest.addressId );
+  const consultAddress = await allRepositories.consultAddressRepository(
+    lastRequest.addressId
+  );
   if (!consultAddress) throw new NotFoundError("Address não existe");
-
-  if(lastRequest.products.length  === 0) throw new NotFoundError("products não existe");
+console.log(consultAddress)
+  if (lastRequest.products.length === 0) {
+    throw new NotFoundError("products não existe");
+  }
+  if (consultAddress.userId !== userId) {
+    throw new NotFoundError("endereço pertence a outro usuario");
+  }
 
   const productsBody = lastRequest.products;
   for (let i = 0; i < productsBody.length; i++) {
-    const productsLocals = await allRepositories.consultProductsHotRepository(productsBody[i].id);
-    if (productsLocals.length === 0) throw new NotFoundError(`productId:${productsBody[i].id} não existe`);
+    const productsLocals = await allRepositories.consultProductsHotRepository(
+      productsBody[i].id
+    );
+    if (productsLocals.length === 0) {
+      throw new NotFoundError(`productId:${productsBody[i].id} não existe`);
+    }
   }
 
   const result = await allRepositories.postRequestRepository(lastRequest);
@@ -30,4 +43,10 @@ async function getRequestService(id: number) {
   return result;
 }
 
-export { postRequestService, getRequestService };
+async function getAllRequestService() {
+  const result = await allRepositories.getAllRequestRepository();
+
+  return result;
+}
+
+export { postRequestService, getRequestService, getAllRequestService };
