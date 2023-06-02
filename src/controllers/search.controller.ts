63ca78daf89cde5031bf60e7f36e827a13112f55
GiovanneAndrade <vanne.as@hotmail.com>
2023-06-01 22:31:42 -0300

    :sparkles:	feat:  search controller

---
+++ src/controllers/search.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import * as allUsers from "../services";
+import { InternalServerError, ifNotFoundError } from "../erros/erros";
+
+
+interface SearchQuery {
+  term: string;
+}
+async function postSearchController(req: Request<{}, {}, {}, SearchQuery>, res: Response)  {
+ 
+  const { term } = req.query;
+  try {
+    const result = await allUsers.postSearchService(term);
+    return res.send(result);
+  } catch (error) {
+    return InternalServerError(res);
+  }
+}
+ 
+
+export { postSearchController };
