0e115e7ba6856266f83a3b2666fad4b07fbef894
GiovanneAndrade <vanne.as@hotmail.com>
2023-06-01 22:29:52 -0300

    :sparkles:	feat:  search service

---
+++ src/services/search.services.ts
@@ -0,0 +1,12 @@
+import { User } from "../types";
+import * as allRepositories from "../repositories";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { NotFoundError } from "../erros/erros";
+
+async function postSearchService(term: string) {
+  const result = await allRepositories.postSearchRepository(term);
+  return result;
+}
+
+export { postSearchService };
