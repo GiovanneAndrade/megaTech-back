2655b19d5d7ab7cd3406fce9315633585ef3c166
GiovanneAndrade <vanne.as@hotmail.com>
2023-06-01 22:37:11 -0300

    :ok_hand:	style: catching errors and leaving dynamic userId

--- src/controllers/address.controllers.ts
+++ src/controllers/address.controllers.ts
@@ -1,15 +1,17 @@
 import { Request, Response } from "express";
 import * as allAddressService from "../services";
-import { Andress } from "@/protocols";
-import { 
-  ifNotFoundError, InternalServerError,  ifUnauthoredError
+import { Andress } from "../types";
+import {
+  ifNotFoundError,
+  InternalServerError,
+  ifUnauthoredError,
 } from "../erros/erros";
 
 async function postAddressController(req: Request, res: Response) {
   const newAddress = req.body as Andress;
-   
+  const userId = req.user.userId;
   try {
-    const result = await allAddressService.postAddressServices(newAddress);
+    const result = await allAddressService.postAddressServices(newAddress, userId);
     return res.send(result);
   } catch (error: any) {
     if (error.statusCode === 404) return ifNotFoundError(res, error);
@@ -19,8 +21,9 @@ async function postAddressController(req: Request, res: Response) {
 }
 
 async function getAddressController(req: Request, res: Response) {
+  const userId = req.user.userId;
   try {
-    const result = await allAddressService.getAddressServices();
+    const result = await allAddressService.getAddressServices(Number(userId));
     return res.send(result);
   } catch (error) {
     return InternalServerError(res);
@@ -29,9 +32,9 @@ async function getAddressController(req: Request, res: Response) {
 
 async function deleteAddressController(req: Request, res: Response) {
   const id = req.params.id as string;
-
+  const userId = req.user.userId;
   try {
-    await allAddressService.deleteAddressServices(id);
+    await allAddressService.deleteAddressServices(id, userId);
     return res.sendStatus(200);
   } catch (error: any) {
     if (error.statusCode === 401) return ifUnauthoredError(res, error);
@@ -43,10 +46,12 @@ async function deleteAddressController(req: Request, res: Response) {
 async function updateAddressController(req: Request, res: Response) {
   const { currentAddress, previousAddress } = req.params;
 
+  const userId = req.user.userId;
   try {
     const result = await allAddressService.updateAddressServices(
       currentAddress,
-      previousAddress
+      previousAddress,
+      userId
     );
     return res.send(result);
   } catch (error: any) {
