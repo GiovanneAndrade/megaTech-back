bd7b9676c48ad362748ba8a486a9cd66d2627ced
GiovanneAndrade <vanne.as@hotmail.com>
2023-06-07 20:04:19 -0300

    :sparkles:	fix: add quantity rule for a product in the order

--- src/services/request.services.ts
+++ src/services/request.services.ts
@@ -2,6 +2,8 @@ import { NotFoundError } from "../erros/erros";
 import { Requests } from "../types";
 import * as allRepositories from "../repositories";
 import { consultUserService } from "./consultUser.services";
+import cron from "node-cron";
+import dayjs from "dayjs";
 
 async function postRequestService(lastRequest: Requests, userId: number) {
   if (lastRequest.userId !== userId) {
@@ -31,6 +33,20 @@ console.log(consultAddress)
       throw new NotFoundError(`productId:${productsBody[i].id} não existe`);
     }
   }
+  
+  const request = await allRepositories.postRequestRepository(lastRequest);
+  const updatedProductQuantities = lastRequest.productQuantities.map((productQuantity) => ({
+    ...productQuantity,
+    requestId: request.id
+  }));
+  
+  const result = await Promise.all(updatedProductQuantities.map((productQuantity) => {
+     allRepositories.postQuantityRepository(productQuantity);
+     return allServices.updateStokeProductsService(productQuantity);
+  }));
+  
+  return {...request, result};
+}
 
   const result = await allRepositories.postRequestRepository(lastRequest);
   return result;
