80a57e42de493696a287d2dafd1f3f508a016520
GiovanneAndrade <vanne.as@hotmail.com>
2023-06-07 19:58:30 -0300

    :sparkles:	fix: add rules to favorites service

--- src/services/favorities.services.ts
+++ src/services/favorities.services.ts
@@ -1,13 +1,21 @@
 import { NotFoundError } from "../erros/erros";
-import { Favorities } from "@/protocols";
+import { Favorities } from "../types";
 import * as allFavorities from "../repositories";
 import { consultUserService } from "./consultUser.services";
 
-async function postFavoritiesService(favorities: Favorities) {
-  await consultUserService(favorities.userId)
+async function postFavoritiesService(favorities: Favorities, userId: number) {
+  console.log(favorities.userId, userId)
+  if (favorities.userId !== userId) {
+    throw new NotFoundError("não autorizado");
+  }
+
+  await consultUserService(favorities.userId);
   const { id } = favorities.productId[0];
   const consultProductId = await allFavorities.consultProductsHotRepository(id);
-  if (consultProductId.length === 0) throw new NotFoundError("produto não existe");
+
+  if (consultProductId.length === 0) {
+    throw new NotFoundError("produto não existe");
+  }
 
   const result = await allFavorities.postFavoritiesRepository(favorities);
   return result;
