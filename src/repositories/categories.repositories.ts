da697e1e15427ca0afe72fd7ab1e934bb22c0245
GiovanneAndrade <vanne.as@hotmail.com>
2023-06-13 16:21:55 -0300

    :sparkles:	feat:  add price history in category repository

--- src/repositories/categories.repositories.ts
+++ src/repositories/categories.repositories.ts
@@ -2,13 +2,32 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function getCategoryRepository() {
+async function getCategoryRepository(  ) {
+  
   const result = await prisma.category.findMany({
     select: {
       id: true,
       name: true,
       image: true,
-      products: true,
+      products: {
+        select:{
+          id: true,
+          image:true,
+          name:true,
+          price:true,
+          stoke:true,
+          Assessments:true,
+          description:true, 
+          PriceHistory:{
+            select:{
+              newPrice:true,
+              previousPrice:true
+            }
+          }
+        },
+        skip: 1,
+        take: 10,
+      },
     },
   });
 
