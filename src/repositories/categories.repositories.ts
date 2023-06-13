79b4730a8d5da521b9ca59c8ae29797a91f975e3
GiovanneAndrade <vanne.as@hotmail.com>
2023-06-13 16:22:54 -0300

    :sparkles:	feat: page get by category repository

--- src/repositories/categories.repositories.ts
+++ src/repositories/categories.repositories.ts
@@ -33,5 +33,38 @@ async function getCategoryRepository(  ) {
 
   return result;
 }
+async function getPageCategoryRepository (page:any,categoryId:any) {
+  const skip = (Number(page) - 1) * 10;
+  const result = await prisma.category.findMany({
+    where:{
+      id:Number(categoryId)
+    },
+    select: {
+      id: true,
+      name: true,
+      image: true,
+      products: {
+        select:{
+          id: true,
+          image:true,
+          name:true,
+          price:true,
+          stoke:true,
+          Assessments:true,
+          description:true, 
+          PriceHistory:{
+            select:{
+              newPrice:true,
+              previousPrice:true
+            }
+          }
+        },
+        skip: skip,
+        take: 10,
+      },
+    },
+  });
 
-export { getCategoryRepository };
+  return result[0];
+}
+export { getCategoryRepository, getPageCategoryRepository };
