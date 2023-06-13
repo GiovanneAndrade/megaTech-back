9cdb273b9e609bc492fab06a8d872c389a4c1fa8
GiovanneAndrade <vanne.as@hotmail.com>
2023-06-13 16:24:23 -0300

    :sparkles:	fix: tweaks to controller favorites answers

--- src/controllers/favorities.controllers.ts
+++ src/controllers/favorities.controllers.ts
@@ -1,14 +1,28 @@
 import { Request, Response } from "express";
 import { ifNotFoundError, InternalServerError } from "../erros/erros";
-import { Favorities } from "@/protocols";
+import { Favorities } from "../types";
 import * as allFavorities from "../services";
 
 async function postFavoritiesController(req: Request, res: Response) {
   const favorities = req.body as Favorities;
+  const userId = req.user.userId;
   try {
-    await allFavorities.postFavoritiesService(favorities);
-    return res.sendStatus(201);
-  } catch (error:any) {
+    const result = await allFavorities.postFavoritiesService(favorities, userId);
+    return res.send(result[0]);
+  } catch (error: any) {
+    if (error.statusCode === 404) return ifNotFoundError(res, error);
+
+    return InternalServerError(res);
+  }
+}
+
+async function putRemoveFavoritiesController(req: Request, res: Response) {
+  const favorities = req.body as Favorities;
+  const userId = req.user.userId;
+  try {
+    const result = await allFavorities.postRemoveFavoritiesService(favorities, userId);
+    return res.sendStatus(200);
+  } catch (error: any) {
     if (error.statusCode === 404) return ifNotFoundError(res, error);
 
     return InternalServerError(res);
@@ -16,16 +30,20 @@ async function postFavoritiesController(req: Request, res: Response) {
 }
 
 async function getFavoritiesController(req: Request, res: Response) {
-  const  userId  = req.params.id as string;
+  const userId = req.user.userId;
   try {
     const result = await allFavorities.getFavoritiesService(Number(userId));
     const { products } = result[0];
     return res.send(products);
-  } catch (error:any) {
+  } catch (error: any) {
     if (error.statusCode === 404) return ifNotFoundError(res, error);
-    
+
     return InternalServerError(res);
   }
 }
 
-export { postFavoritiesController, getFavoritiesController };
+export {
+  postFavoritiesController,
+  getFavoritiesController,
+  putRemoveFavoritiesController,
+};
