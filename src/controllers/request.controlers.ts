1302c50a9c7abdbd89f24294981833c679ddb83e
GiovanneAndrade <vanne.as@hotmail.com>
2023-06-13 16:26:22 -0300

    :sparkles:	fix:  adjustments to the order controller userId

--- src/controllers/request.controlers.ts
+++ src/controllers/request.controlers.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from "express";
 import { ifNotFoundError, InternalServerError } from "../erros/erros";
-import { Requests } from "@/protocols";
+import { Requests } from "../types";
 import * as allRequest from "../services";
 
 async function postRequestController(req: Request, res: Response) {
   const lastRequest = req.body as Requests;
-
+  const userId = req.user.userId;
   try {
-    const summary = await allRequest.postRequestService(lastRequest);
+    const summary = await allRequest.postRequestService(lastRequest, userId);
     return res.send(summary);
   } catch (error: any) {
     if (error.statusCode === 404) return ifNotFoundError(res, error);
@@ -17,9 +17,9 @@ async function postRequestController(req: Request, res: Response) {
 }
 
 async function getRequestController(req: Request, res: Response) {
-  const id = req.params.id;
+  const userId = req.user.userId;
   try {
-    const result = await allRequest.getRequestService(Number(id));
+    const result = await allRequest.getRequestService(Number(userId));
     return res.send(result);
   } catch (error: any) {
     if (error.statusCode === 404) return ifNotFoundError(res, error);
@@ -28,4 +28,15 @@ async function getRequestController(req: Request, res: Response) {
   }
 }
 
-export { postRequestController, getRequestController };
+async function getAllRequestController(req: Request, res: Response) {
+  try {
+    const result = await allRequest.getAllRequestService();
+    return res.send(result);
+  } catch (error: any) {
+    if (error.statusCode === 404) return ifNotFoundError(res, error);
+
+    return InternalServerError(res);
+  }
+}
+
+export { postRequestController, getRequestController, getAllRequestController };
