953916940fdc9d5c465801722c2dd539881b98e0
GiovanneAndrade <vanne.as@hotmail.com>
2023-06-13 16:25:14 -0300

    :sparkles:	feat: put product hot controller

--- src/controllers/products.controllers.ts
+++ src/controllers/products.controllers.ts
@@ -3,6 +3,8 @@ import { InternalServerError } from "../erros/erros";
 import * as allProducts from "../services";
 
 async function getProductsController(req: Request, res: Response) {
+  const userId = req.user.userId;
+
   try {
     const result = await allProducts.getProductsService();
     return res.send(result);
@@ -10,7 +12,6 @@ async function getProductsController(req: Request, res: Response) {
     return InternalServerError(res);
   }
 }
-
 
 async function getProductsHotController(req: Request, res: Response) {
   try {
@@ -20,4 +21,19 @@ async function getProductsHotController(req: Request, res: Response) {
     return InternalServerError(res);
   }
 }
-export { getProductsController, getProductsHotController };
+
+async function updateProductsHotController(req: Request, res: Response) {
+  const { newPrice, previousPrice, productId } = req.body as any;
+  try {
+    const result = await allProducts.updateHistoricService(newPrice, productId);
+    return res.send(result);
+  } catch (error) {
+    return InternalServerError(res);
+  }
+}
+
+export {
+  getProductsController,
+  getProductsHotController,
+  updateProductsHotController,
+};
