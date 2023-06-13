09f49359bf9ee274c2ecdf08eb6718a8157ba04b
GiovanneAndrade <vanne.as@hotmail.com>
2023-06-13 16:14:14 -0300

    :sparkles:	feat: put page product router

--- src/routers/products.router.ts
+++ src/routers/products.router.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import {
   getProductsController,
   getProductsHotController,
+  updateProductsHotController,
 } from "../controllers";
 
 const productsRouter = Router();
 productsRouter
   .get("/product", getProductsController)
-  .get("/product/hot", getProductsHotController);
-
+  .get("/product/hot", getProductsHotController)
+  .put("/product", updateProductsHotController)
 export default productsRouter;
