658030d3aeca627fa257a218cbb05d17fe3eec11
GiovanneAndrade <vanne.as@hotmail.com>
2023-06-13 16:16:07 -0300

    :sparkles:	feat: get Search Repository

---
+++ src/repositories/search.repository.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function postSearchRepository(term: string) {
+  const products = await prisma.products.findMany({
+    where: {
+      OR: [
+        {
+          name: {
+            contains: term,
+            mode: "insensitive",
+          },
+        },
+        {
+          description: {
+            contains: term,
+            mode: "insensitive",
+          },
+        },
+      ],
+    },
+    select: {
+      id: true,
+      image: true,
+      name: true,
+      price: true,
+      stoke: true,
+      Assessments: true,
+      description: true,
+      PriceHistory: {
+        select: {
+          newPrice: true,
+          previousPrice: true,
+        },
+      },
+    },
+  });
+
+  return products;
+}
+
+export { postSearchRepository };
