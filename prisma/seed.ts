02fa3905784d1640f7103ab6c0d62ce468460475
GiovanneAndrade <vanne.as@hotmail.com>
2023-06-22 22:39:20 -0300

    :sparkles:	fix:  adjust the seed that now creates the images

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,45 +4,55 @@ import fs from "fs/promises";
 
 const prisma = new PrismaClient();
 
-async function seed() {
-  try {
-    await prisma.priceHistory.deleteMany();
-    await prisma.productQuantity.deleteMany(); 
-    await prisma.products.deleteMany();  
-    await prisma.category.deleteMany();  
-    await prisma.productImage.deleteMany(); 
-    await prisma.favorities.deleteMany(); 
- 
-    const filePath = path.join(__dirname, "dados.json");
-    const data = await fs.readFile(filePath, "utf8");
-    const categoriesProducts = JSON.parse(data);
+async function seedFile(filename:any) {
+  const filePath = path.join(__dirname, "data", filename);
+  const data = await fs.readFile(filePath, "utf8");
+  const categoriesProducts = JSON.parse(data);
 
-    for (const categoryProduct of categoriesProducts) {
-      const { name, image, products } = categoryProduct;
+  for (const categoryProduct of categoriesProducts) {
+    const { name, image, products } = categoryProduct;
 
-      const createdCategory = await prisma.category.create({
+    const createdCategory = await prisma.category.create({
+      data: {
+        name,
+        image,
+      },
+    });
+    for (const product of products) {
+      const createdProduct = await prisma.products.create({
         data: {
-          name,
-          image,
-        },
-      });
-
-      for (const product of products) {
-        const createdProduct = await prisma.products.create({
-          data: {
-            name: product.name,
-            price: product.price,
-            description: product.description,
-            Assessments: product.Assessments,
-            category: {
-              connect: {
-                id: createdCategory.id,
-              },
+          name: product.name,
+          price: product.price,
+          description: product.description,
+          Assessments: "",
+          stoke: 50,
+          category: {
+            connect: {
+              id: createdCategory.id,
             },
           },
-        });
-      }
+          images: product.images ? {
+            create: product.images.map((image:any) => ({ url: image })),
+          } : {},
+        },
+      });
     }
+  }
+}
+
+async function seed() {
+  try {
+    await prisma.productImage.deleteMany();
+    await prisma.priceHistory.deleteMany();
+    await prisma.productQuantity.deleteMany();
+    await prisma.products.deleteMany();
+    await prisma.category.deleteMany();
+    await prisma.favorities.deleteMany();
+
+    await seedFile("smartphones.json");
+    await seedFile("Notebooks.json");
+    await seedFile("smartwatch.json");
+    await seedFile("all-inOne.json");
 
     console.log("Seed completed successfully.");
   } catch (error) {
