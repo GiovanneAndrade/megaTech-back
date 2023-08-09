f4312609b867082a86bdefa23e2dbca443090da3
GiovanneAndrade <vanne.as@hotmail.com>
2023-08-09 15:54:20 -0300

    :sparkles:	feat:  add swagger index

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ import "express-async-errors";
 import cors from 'cors';
 import express from "express";
 import router from "./routers/index.router";
-
-
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(router)
    
 
