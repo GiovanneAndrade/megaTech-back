0885daef370e2fbed3d3b5c628e14d644be859db
GiovanneAndrade <vanne.as@hotmail.com>
2024-02-02 14:36:53 -0300

    :sparkles:	fix: import adjustments

--- src/routers/index.router.ts
+++ src/routers/index.router.ts
@@ -6,18 +6,21 @@ import addressRouter from "@/routers/address.router";
 import favoritiesRouter from "@/routers/favorities.router";
 import resquestRouter from "@/routers/request.router";
 import productsRouter from "@/routers/products.router";
-import searchRouter from "./search.router";
-import contactRouter from "./contact.router"
+import searchRouter from "@/routers/search.router";
+import contactRouter from "@/routers/contact.router"
+import historicRouter from "@/routers/historic.routers";
+
 const router = Router();
 
-router.use(searchRouter);
-router.use(userRouter);
-router.use(categoriesRouter);
-router.use(addressRouter);
-router.use(favoritiesRouter);
-router.use(resquestRouter);
-router.use(productsRouter);
-router.use(contactRouter);
+  router.use(searchRouter);
+  router.use(userRouter);
+  router.use(categoriesRouter);
+  router.use(addressRouter);
+  router.use(favoritiesRouter);
+  router.use(resquestRouter);
+  router.use(productsRouter);
+  router.use(contactRouter);
+  router.use(historicRouter);
 
 router.use(handleApplicationErrors);
 
